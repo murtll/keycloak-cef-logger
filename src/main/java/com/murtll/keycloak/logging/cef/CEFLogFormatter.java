af78c95b10e774c574e226e203b735f9fc96868f
murtll <jkoz@inbox.ru>
2023-10-30 23:26:12 +0300

    upd event formatter

--- src/main/java/com/murtll/keycloak/logging/cef/CEFLogFormatter.java
+++ src/main/java/com/murtll/keycloak/logging/cef/CEFLogFormatter.java
@@ -1,36 +1,81 @@
 package com.murtll.keycloak.logging.cef;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.keycloak.events.Event;
 import org.keycloak.events.admin.AdminEvent;
 
 public class CEFLogFormatter {
 
+    private String cefDeviceVendor;
+    private String cefDeviceProduct;
+    private String cefDeviceVersion;
+
+    public CEFLogFormatter() {
+        this.cefDeviceVendor  = "Keycloak";
+        this.cefDeviceProduct = "Keycloak";
+        this.cefDeviceVersion = "20.0.1";
+    }
+
+    public CEFLogFormatter(String cefDeviceVendor, String cefDeviceProduct, String cefDeviceVersion) {
+        this.cefDeviceVendor  = cefDeviceVendor;
+        this.cefDeviceProduct = cefDeviceProduct;
+        this.cefDeviceVersion = cefDeviceVersion;
+    }
+
     public String format(Event e) {
+        return this.formatGeneric(e.getType().getStableIndex(),
+            e.getType().name(),
+            this.getSeverity(e.getType().getStableIndex()),
+            e.getDetails());
+    }
+
+    public String format(AdminEvent e) {
+        int eventId = Integer.parseInt(e.getOperationType().getStableIndex() + "" + e.getResourceType().ordinal());
+        String eventName = e.getOperationType() + "_" + e.getResourceType();
+
+        return this.formatGeneric(eventId,
+            eventName,
+            this.getSeverity(eventId),
+            new HashMap<>());
+    }
+
+    private String getSeverity(int index) {
+        if (index > 10) {
+            return "Medium";
+        } else {
+            return "Low";
+        }
+    }
+
+    private String formatGeneric(int eventId, String eventType, String severity, Map<String, String> details) {
         StringBuilder s = new StringBuilder();
 
-        s.append("CEF:0|Keycloak|Keycloak|20.0.1|")
-            .append(e.getId()).append("|")
-            .append(e.getType()).append("|")
-            .append("Medium").append("|");
+        s.append("CEF:0|")
+            .append(cefDeviceVendor).append("|")
+            .append(cefDeviceProduct).append("|")
+            .append(cefDeviceVersion).append("|")
+            .append(eventId).append("|")
+            .append(eventType).append("|")
+            .append(severity).append("|");
 
-        e.getDetails().forEach((k, v) -> {
-            s.append(k).append('=').append(v).append(' ');
+        details.forEach((k, v) -> {
+            s.append(k).append('=').append(v.replace(' ', '_')).append(' ');
         });
 
         return s.toString();
     }
 
-    public String format(AdminEvent e) {
-        StringBuilder s = new StringBuilder();
-
-        s.append("CEF:0|Keycloak|Keycloak|20.0.1|")
-            .append(e.getId()).append("|")
-            .append(e.getOperationType().name()).append("|")
-            .append("Medium").append("|");
-
-        s.append("admin event type, no details 4 now");
-
-        return s.toString();
+    public String getCefDeviceVendor() {
+        return this.cefDeviceVendor;
     }
 
+    public String getCefDeviceProduct() {
+        return this.cefDeviceProduct;
+    }
+
+    public String getCefDeviceVersion() {
+        return this.cefDeviceVersion;
+    }
 }
